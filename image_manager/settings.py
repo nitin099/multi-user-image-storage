7fa7796a5244656dcd593c6a73db53c3da0f2cdb
nitin099 <nitin@appsforbharat.com>
2024-12-11 13:54:38 +0530

    add swagger doc

--- image_manager/settings.py
+++ image_manager/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "images",
-    "rest_framework"
-,]
+    "rest_framework",
+    "drf_yasg",
+]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
